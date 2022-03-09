804f105906cc102cc2e760e55ec86c4eb98267c5
Aleksandr <aleksandrnaumenko@mail.ru>
2022-03-09 13:15:08 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import {SignInRender} from './pages/SignIn/SignIn.js';
+import {getCookie} from './modules/GetCookie/GetCookie.js';
+import {MainPage} from './pages/MainPage/MainPage.js';
+import {Ajax} from './modules/AjaxSignIn/AjaxSignIn.js';
 
 const root = document.getElementsByTagName('body')[0];
 
-const main = new SignInRender(root);
-main.render();
+if (getCookie('email') === undefined || getCookie('password')=== undefined) {
+    const signIn = new SignInRender(root);
+    signIn.render();
+}
+else {
+    const email = getCookie('email');
+    const password = getCookie('password');
+    const ajaxSignIn = new Ajax();
+    ajaxSignIn.post(
+        'http://127.0.0.1:8080/signin',
+        (status, responseText) => {
+            if (status != 200) {
+                const signIn = new SignInRender(root);
+                signIn.render();
+                return;
+            }
+
+            const parsed = JSON.parse(responseText);
+            if (parsed['status'] == 0) {
+                const main = new MainPage(parent);
+                main.render();
+            }
+            else {
+                const signIn = new SignInRender(root);
+                signIn.render();
+            }
+        },
+        {
+            'email': email,
+            'password': password,
+        },
+    );
+}
+
